108d9bbffc8ea3b0cfa4afcd92f5c8d8d91124e8
I-Uddin <imran.live@hotmail.com>
2023-02-18 21:28:09 +0000

    associations made and layout for tests created.

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const { User } = require("./Models/User");
 const { Board } = require("./Models/Board");
 const { Cheese } = require("./Models/Cheese");
 
+// Users < - > Boards
+Board.belongsTo(User);
+User.hasMany(Board);
 
+// Boards < - > Cheeses
+Board.belongsToMany(Cheese, { through: "cheese_boards" });
+Cheese.belongsToMany(Board, { through: "cheese_boards" });
 
 module.exports = { User, Board, Cheese };
--- index.test.js
+++ index.test.js
@@ -3,15 +3,14 @@ const { User, Board, Cheese } = require("./index");
 const { seedUser, seedBoard, seedCheese } = require("./seedData");
 
 describe("Model Tests", () => {
-
   beforeAll(async () => {
     await sequelize.sync({ force: true });
   });
 
   beforeEach(async () => {
-    const user1 = await User.create( seedUser[0] );
-    const board1 = await Board.create( seedBoard[0] );
-    const cheese1 = await Cheese.create( seedCheese[0] );
+    const user1 = await User.create(seedUser[0]);
+    const board1 = await Board.create(seedBoard[0]);
+    const cheese1 = await Cheese.create(seedCheese[0]);
   });
 
   afterEach(async () => {
@@ -27,23 +26,90 @@ describe("Model Tests", () => {
   it("can create a User", async () => {
     const userFound = await User.findByPk(1);
     expect(userFound.id).toBe(1);
-    expect(userFound.name).toBe( seedUser[0].name );
-    expect(userFound.email).toBe( seedUser[0].email );
+    expect(userFound.name).toBe(seedUser[0].name);
+    expect(userFound.email).toBe(seedUser[0].email);
   });
 
   it("can create a Board", async () => {
     const boardFound = await Board.findByPk(1);
     expect(boardFound.id).toBe(1);
-    expect(boardFound.type).toBe( seedBoard[0].type );
-    expect(boardFound.description).toBe( seedBoard[0].description );
-    expect(boardFound.rating).toBe( seedBoard[0].rating );
+    expect(boardFound.type).toBe(seedBoard[0].type);
+    expect(boardFound.description).toBe(seedBoard[0].description);
+    expect(boardFound.rating).toBe(seedBoard[0].rating);
   });
 
   it("can create a Cheese", async () => {
     const cheeseFound = await Cheese.findByPk(1);
     expect(cheeseFound.id).toBe(1);
-    expect(cheeseFound.title).toBe( seedCheese[0].title );
-    expect(cheeseFound.description).toBe( seedCheese[0].description );
+    expect(cheeseFound.title).toBe(seedCheese[0].title);
+    expect(cheeseFound.description).toBe(seedCheese[0].description);
+  });
+});
+
+describe("Association Tests", () => {
+  beforeAll(async () => {
+    await sequelize.sync({ force: true });
   });
 
+  beforeEach(async () => {
+    const user1 = await User.create(seedUser[0]);
+    const board1 = await Board.create(seedBoard[0]);
+    const cheese1 = await Cheese.create(seedCheese[0]);
+  });
+
+  afterEach(async () => {
+    await User.sync({ force: true });
+    await Board.sync({ force: true });
+    await Cheese.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.drop();
+  });
+
+  it("check multiple Boards can be added to a User.", async () => {
+
+  });
+
+  it("checks a Board can have many Cheeses", async () => {
+
+  });
+
+  it("checks a Cheese can be on many Boards", async () => {
+
+  });
+});
+
+describe("Eager Loading Tests", () => {
+  beforeAll(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  beforeEach(async () => {
+    const user1 = await User.create(seedUser[0]);
+    const board1 = await Board.create(seedBoard[0]);
+    const cheese1 = await Cheese.create(seedCheese[0]);
+  });
+
+  afterEach(async () => {
+    await User.sync({ force: true });
+    await Board.sync({ force: true });
+    await Cheese.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.drop();
+  });
+
+  it("checks board can be loaded with its cheeses", async () => {
+
+  });
+
+  it("checks user can be loaded with its boards", async () => {
+
+  });
+
+  it("checks cheese can be loaded with its board data", async () => {
+
+  });
 });
